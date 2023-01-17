02639cf7444906f2fc06115ca2eebeef02d50461
Izhar Ulhaque <codewithizhar@gmail.com>
2023-01-17 23:20:46 +0530

    first login setup

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let videos = document.querySelectorAll('video');
+let closeButtons = document.querySelectorAll('.btn-close');
+
+closeButtons.forEach(function(btn, i){
+	btn.addEventListener('click', function(e){
+		videos[i].pause();
+	})
+})
+
+// ======================updated
+let user = {
+		username:'admin',
+		password:'8975741028'
+	};
+
+
+let username = document.querySelector('#username');
+let password = document.querySelector('#password');
+let loginBtn = document.querySelector('#authenticate')
+let loginWIndow = document.querySelector('#loginWIndow');
+let mainWindow =  document.querySelector('#mainWindow');
+
+loginBtn.addEventListener('click', function(e){
+	e.preventDefault();
+
+	if(username.value === user.username){
+		if(password.value === user.password ){
+			// console.log('login success')
+			window.location = './assets/index.html'
+		}else{
+			alert('Incorrect password!')
+		}
+	}else{
+		alert('Incorrect username!')
+	}
+
+});
---
+++ assets/video/Readme.txt
@@ -0,0 +1,27 @@
+Add these two video inside video folder 
+and rename them as written below
+
+hot-cross-buns.mp4
+ilovemyschool.mp4
+mondays-child.mp4
+the-itsy-bitsy-spider.mp4
+shape-song.mp4
+stamp-your-feet.mp4
+little-pumpkin.mp4
+this-is-the-way.mp4
+mondays-child.mp4
+he-diddle-diddle.mp4
+traffic-light-song.mp4
+/////////////////////////////////////////////////
+london-bridge.mp4
+chintu.mp4
+maa.mp4
+billi-mosi.mp4
+mummy-ki-mummy-ho-tum.mp4
+rail.mp4
+laal-peeli-motor.mp4
+chuhiya-rani.mp4
+hathi-aaya.mp4
+sadak.mp4
+nav-hamari.mp4
+ek-dal-par-baitha-bandar.mp4
